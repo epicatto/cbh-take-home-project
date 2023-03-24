f9ce32b0243a285482adbf9c7828e04c9ef0a404
epicatto <epicatto@hotmail.com>
2023-03-24 11:29:23 -0300

    CBH: Take home project
    * Add unit test scenarios.
    * Add unit test coverage report.

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,65 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns the given partitionKey if it is present, is an string and its length is less or equals to 256", () => {
+    const event = {
+      partitionKey: "TEST_PARTITION_KEY",
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBeDefined();
+    expect(trivialKey).toBe(event.partitionKey);
+  });
+
+  it("Returns the given partitionKey (string) if it is present, is not an string and its length is less or equals to 256", () => {
+    const event = {
+      partitionKey: {
+        prop4: 0,
+        prop5: null,
+      },
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBeDefined();
+    expect(trivialKey).toBe(JSON.stringify(event.partitionKey));
+  });
+
+  it("Returns the HASH of given partitionKey if it is present, is an string and its length is greater than 256", () => {
+    const event = {
+      // 257 characters
+      partitionKey:
+          "uinxu7cypnwvr2paxvf1h6vtn02xyftneutdycvh2at20awgaxp5asib2pwjow6nkxdu1bx8hfrsr9cvmvvvxdfpa6wxcdditgpscowbh4jjdo73nfpeais8fgsv4mfjgqynkba0shyswror8dwghwrwbnpd7gvu4jysdmhkygfqtdxpadgiwvqixf3yfr2ehuoitvqdip7tydc1xhueqx9fzzwze3njy9ki8jqtfzz5esde9wsgaln8hsxqiuruf",
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBeDefined();
+    expect(trivialKey).not.toBe(event.partitionKey);
+  });
+
+  it("Returns the HASH of given partitionKey if it is present, is not an string and its length is greater than 256", () => {
+    const event = {
+      partitionKey: {
+        // 257 characters
+        prop4:
+            "uinxu7cypnwvr2paxvf1h6vtn02xyftneutdycvh2at20awgaxp5asib2pwjow6nkxdu1bx8hfrsr9cvmvvvxdfpa6wxcdditgpscowbh4jjdo73nfpeais8fgsv4mfjgqynkba0shyswror8dwghwrwbnpd7gvu4jysdmhkygfqtdxpadgiwvqixf3yfr2ehuoitvqdip7tydc1xhueqx9fzzwze3njy9ki8jqtfzz5esde9wsgaln8hsxqiuruf",
+        prop5: null,
+      },
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBeDefined();
+    expect(trivialKey).not.toBe(event.partitionKey);
+    expect(trivialKey).not.toBe(JSON.stringify(event.partitionKey));
+  });
+
+  it("Returns the HASH of given input if the partitionKey is not present", () => {
+    const event = {
+      prop1: "TEST_PROP1",
+      prop2: true,
+      prop3: {
+        prop4: 0,
+        prop5: null,
+      },
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBeDefined();
+    expect(trivialKey.length).toBeLessThanOrEqual(256);
+  });
 });
